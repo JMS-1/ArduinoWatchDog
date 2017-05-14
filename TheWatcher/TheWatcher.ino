9936a551d3bf92b89d1696869d0fca3534bc698b
Dr. Jochen Manns <vcrnet-1@psimarron.net>
2017-05-14 14:39:37 +0200

    Created empty

---
+++ TheWatcher/TheWatcher.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TheWatcher.ino
+ Created:	5/14/2017 2:38:12 PM
+ Author:	Dr. Jochen Manns
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
